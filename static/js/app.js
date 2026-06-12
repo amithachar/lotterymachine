56e5ce6752797f0f505f3af8dab754e0f1e37314
Your Name <your_email@example.com>
2026-06-12 12:23:38 +0530

    new feature added

--- static/js/app.js
+++ static/js/app.js
@@ -1,83 +1,178 @@
+let history=[]
+
 async function startRoll(){
 
-const btn =
+const btn=
 document.getElementById(
 "startBtn"
-);
+)
 
-btn.disabled=true;
+btn.disabled=true
 
-const balls =
+const balls=
 document.querySelectorAll(
 ".ball"
-);
+)
 
 balls.forEach(
+
 ball=>{
+
 ball.classList.add(
 "roll"
-);
-});
+)
 
-const intervals=[];
+}
+
+)
+
+const intervals=[]
 
 balls.forEach(
-(ball,index)=>{
 
-const number =
+(
+ball,
+index
+
+)=>{
+
+const number=
+
 ball.querySelector(
 "span"
-);
+)
 
 intervals[index]=
-setInterval(()=>{
+
+setInterval(
+
+()=>{
 
 number.innerHTML=
+
 Math.floor(
 Math.random()*10
-);
+)
 
-},70);
+},
 
-});
+70
 
-setTimeout(()=>{
+)
+
+}
+
+)
+
+const response=
+
+await fetch(
+"/start"
+)
+
+const data=
+
+await response.json()
+
+setTimeout(
+
+()=>{
 
 balls.forEach(
-(ball,index)=>{
 
-setTimeout(()=>{
+(
+ball,
+index
+
+)=>{
+
+setTimeout(
+
+()=>{
 
 clearInterval(
 intervals[index]
-);
+)
 
 ball.classList.remove(
 "roll"
-);
+)
 
 ball.querySelector(
 "span"
-).innerHTML=
+)
 
-Math.floor(
-Math.random()*10
-);
+.innerHTML=
+
+data.numbers[
+index
+]
 
 if(
+
 index===5
+
 ){
 
-btn.disabled=false;
+btn.disabled=false
+
+updateHistory(
+data.numbers
+)
 
 }
 
 },
+
 index*700
-);
 
-});
+)
 
-},10000);
+}
+
+)
+
+},
+
+10000
+
+)
+
+}
+
+function updateHistory(
+
+numbers
+
+){
+
+history.unshift(
+
+numbers.join(
+" - "
+)
+
+)
+
+if(
+
+history.length>10
+
+)
+
+history.pop()
+
+document
+.getElementById(
+"ticker"
+)
+
+.innerHTML=
+
+history.join(
+
+"&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;"
+
+)
 
 }
