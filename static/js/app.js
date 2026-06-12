120d8da6f29e8e112267561fcaba07638c315703
Your Name <your_email@example.com>
2026-06-12 15:29:43 +0530

    add live winner features

--- static/js/app.js
+++ static/js/app.js
@@ -286,3 +286,159 @@ loadSales,
 60000
 
 )
+
+const people=[
+
+"Aarav",
+"Vivaan",
+"Aditya",
+"Arjun",
+"Rohan",
+"Kiran",
+"Ananya",
+"Pooja",
+"Sneha",
+"Diya",
+
+"Priya",
+"Nikhil",
+"Rahul",
+"Meera",
+"Riya",
+"Vikram",
+"Neha",
+"Krishna",
+"Akash",
+"Sanjana"
+
+]
+
+function randomName(){
+
+return people[
+
+Math.floor(
+
+Math.random()
+
+*
+
+people.length
+
+)
+
+]
+
+}
+
+function randomPrize(){
+
+return (
+
+Math.floor(
+
+1+
+
+Math.random()*50
+
+)
+
+)
+
++
+
+" Lakh"
+
+}
+
+function loadWinners(){
+
+const board=
+
+document.getElementById(
+"winnerList"
+)
+
+board.innerHTML=""
+
+for(
+
+let i=0;
+
+i<10;
+
+i++
+
+){
+
+const state=
+
+states[
+
+Math.floor(
+
+Math.random()
+
+*
+
+states.length
+
+)
+
+]
+
+const row=
+
+document.createElement(
+"div"
+)
+
+row.className=
+
+"winnerCard"
+
+row.innerHTML=`
+
+<div class="winnerName">
+
+${randomName()}
+
+</div>
+
+<div class="winnerState">
+
+📍 ${state}
+
+</div>
+
+<div class="winnerTicket">
+
+🎟 ${randomTicket()}
+
+</div>
+
+<div class="winnerPrize">
+
+💰 ₹${randomPrize()}
+
+</div>
+
+`
+
+board.appendChild(
+row
+)
+
+}
+
+}
+
+loadWinners()
+
+setInterval(
+
+loadWinners,
+
+60000
+
+)
